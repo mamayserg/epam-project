ed81885fb71c83f6d584f70032883586060e5308
SergioMamai <mamai.sm@pg.com>
2022-05-21 22:31:46 +0200

    add pureComponent

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Button extends Component {
+class Button extends React.PureComponent {
   render() {
   return <button type={this.props.type} >{this.props.text}</button>
   }
--- src/components/Title.js
+++ src/components/Title.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Title extends React.Component {
   render() {
-    return React.createElement('div', null, `Hello ${this.props.title}`);
+    return React.createElement('div', null, `Hello, ${this.props.title}`);
   }
 }
 export default Title
