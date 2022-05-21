7253c38a07eef01f655b26627130acb4b779e87c
SergioMamai <mamai.sm@pg.com>
2022-05-21 22:29:42 +0200

    add Search page add tailwind

--- src/App.js
+++ src/App.js
@@ -18,6 +18,9 @@ resetText(defaultText);
 }
 
 return(<>
+ <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
 <form onSubmit={submit}>
 <Input value={colorProps.value} onChange={colorProps.onChange} type='color' />
 <Input value={textProps.value} onChange={textProps.onChange} type='text' />
