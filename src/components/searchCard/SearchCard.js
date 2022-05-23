3a2859b18b118bedc8dff513529237f9a76c14a5
SergioMamai <mamai.sm@pg.com>
2022-05-24 01:12:46 +0200

    added ErrorBoundary

--- src/components/searchCard/SearchCard.js
+++ src/components/searchCard/SearchCard.js
@@ -1,13 +1,19 @@
 import Header from "../layout/Header";
 import SearchBar from "./SearchBar";
+import "./searchCard.css";
+import ErrorBoundary from "../common/ErrorBoundary";
 
 const SearchCard = () => (
-  <div className="bg-gray-300">
-    <Header />
-    <div className="flex justify-center w-full mt-8 mb-24 search-card">
-      <div className="flex flex-col items-start w-3/6">
-        <h1 className="title text-white pb-8">FIND YOUR MOVIE</h1>
-        <SearchBar />
+  <div className="bg-gray-600 search-card--wrap">
+    <div className="search-card">
+      <Header />
+      <div className="flex justify-center w-full mt-6 mb-24 search-card--bar">
+        <div className="flex flex-col items-start w-3/6">
+          <h1 className="title text-white pb-8">FIND YOUR MOVIE</h1>
+          <ErrorBoundary key="ErrorBoundary">
+            <SearchBar  key="dddErrorBoundary"/>
+          </ErrorBoundary>
+        </div>
       </div>
     </div>
   </div>
