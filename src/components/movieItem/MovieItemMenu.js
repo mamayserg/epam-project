d9b61f9ca149bcda5fb0415d29f703958ccdc13f
SergioMamai <mamai.sm@pg.com>
2022-06-05 13:54:45 +0200

    add close menu btn

--- src/components/movieItem/MovieItemMenu.js
+++ src/components/movieItem/MovieItemMenu.js
@@ -38,8 +38,9 @@ export default function MovieItemMenu({ movie }) {
     event.stopPropagation();
   };
 
-  const handleClose = () => {
+  const handleClose = (e) => {
     setAnchorEl(null);
+    e.stopPropagation();
   };
 
   return (
