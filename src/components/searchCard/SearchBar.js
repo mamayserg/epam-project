759825296a1c09e5da98a4efa72878543f22c5f4
SergioMamai <mamai.sm@pg.com>
2022-06-18 15:48:31 +0200

    added useCallback

--- src/components/searchCard/SearchBar.js
+++ src/components/searchCard/SearchBar.js
@@ -1,9 +1,9 @@
-import { useInput } from "../../hooks/use-input";
+// import { useInput } from "../../hooks/use-input";
 import {
   queryParamsKeys,
   queryParamsValues,
 } from "../../constants/searchParams";
-import { useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useQueryParam } from "../../hooks/use-query-param";
 
 const SearchBar = () => {
@@ -11,28 +11,37 @@ const SearchBar = () => {
     queryParamsKeys.SEARCH
   );
   let [searchByParam] = useQueryParam(queryParamsKeys.SEARCH_BY);
+  const [value, setValue] = useState("");
 
-  const [textProps, resetText] = useInput("");
+  const setSearchValue = useCallback(() => {
+    searchByParam === queryParamsValues.TITLE && searchParam
+      ? setValue(searchParam)
+      : setValue("");
+  }, [searchByParam, searchParam]);
 
   useEffect(() => {
-    const searchQuery = searchParam;
-    if (searchByParam === queryParamsValues.TITLE && searchParam) {
-      resetText(searchQuery);
-    }
-    // eslint-disable-next-line
-  }, []);
+    setSearchValue();
+  }, [setSearchValue]);
 
   const search = () => {
     const queryParams = {
       ...allQueryParams,
       [queryParamsKeys.SEARCH_BY]: queryParamsValues.TITLE,
     };
-    setSearchParam(textProps.value, queryParams);
+    setSearchParam(value, queryParams);
+  };
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
   };
 
   return (
     <div className="flex flex-row w-full">
-      <input className="w-full  bg-gray-500 text-white" {...textProps} />
+      <input
+        className="w-full  bg-gray-500 text-white"
+        value={value}
+        onChange={handleChange}
+      />
       <button
         className="uppercase w-40 mr-4 py-2 text-base text-white bg-red-100  hover:bg-opacity-80 "
         onClick={search}
